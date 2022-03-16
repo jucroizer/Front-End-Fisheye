db37fd1af1f6513ff1dddb620cc4adce3e7410a0
jucroizer <81417866+jucroizer@users.noreply.github.com>
2022-03-16 15:29:20 +0100

    recuperation des informations des photographes dans le fichier json et afficahge de leurs informations sur la page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        article.setAttribute('id', id);
+       
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
+        const h3 = document.createElement('h3');
+        h3.textContent = city + ', ' + country;
+
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+
+        const pPrice = document.createElement('p');
+        pPrice.textContent = price + '€/jour';
+
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,8 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+    const getPhotographers = async() => {
+        return await fetch('http://127.0.0.1:5501/data/photographers.json')
+        .then(function(result) { return result.json() })
+        .then(function(data){ return data })
+        .catch(function(error){ console.log('une erreur fetch' + error)})
     }
 
     async function displayData(photographers) {
@@ -37,8 +17,9 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        const photographers  = await getPhotographers();
+        
+        displayData(photographers.photographers);
     };
     
     init();
