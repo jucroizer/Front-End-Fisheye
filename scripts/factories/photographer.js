e368323b7104d5e0c63227cdd5028da1ff0636e6
jucroizer <81417866+jucroizer@users.noreply.github.com>
2022-03-23 15:18:57 +0100

    recuperation des donnees du photographe et de ses oeuvres

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,13 @@
 function photographerFactory(data) {
-    const { name, portrait, id, city, country, tagline, price } = data;
+    console.log(data);
+    const { name, id, city, country,  tagline, price, portrait, photographerId, 
+        title, image, likes, date } = data;
 
     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+    const media = `assets/photographers/${photographerId}/${image}`;
+    console.log(media);
+    
+    
 
     function getUserCardDOM() {
 
@@ -10,7 +16,7 @@ function photographerFactory(data) {
         
        
         const a = document.createElement('a');
-        a.setAttribute('href', 'http://127.0.0.1:5501/photographer.html#`${id}`');
+        a.setAttribute('href', 'http://127.0.0.1:5501/photographer.html?id=' + id);
 
         const img = document.createElement('img');
         img.setAttribute("src", picture);
@@ -22,12 +28,20 @@ function photographerFactory(data) {
         const h3 = document.createElement('h3');
         h3.textContent = city + ', ' + country;
         h3.style.color = "#901C1C";
+        h3.style.fontWeight = 400;
+        h3.style.fontSize = '13px';
+        h3.style.marginTop = '-10%';
 
         const pTagline = document.createElement('p');
         pTagline.textContent = tagline;
+        pTagline.style.fontSize = '10px';
+        pTagline.style.marginTop = '-3%';
 
         const pPrice = document.createElement('p');
         pPrice.textContent = price + '€/jour';
+        pPrice.style.fontSize = '9px';
+        pPrice.style.color = '#757575';
+        pPrice.style.marginTop = '-1%';
 
         article.appendChild(a);
         a.appendChild(img);
@@ -37,5 +51,59 @@ function photographerFactory(data) {
         article.appendChild(pPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    function getPhotographerMeta() {
+
+        const mainDiv = document.createElement('div');
+        mainDiv.setAttribute('class', 'photograph-meta');
+
+        const divProfile = document.createElement('div');
+        divProfile.setAttribute("class", 'photographer-profile');
+
+        const divPortrait = document.createElement('div');
+        divPortrait.setAttribute("class", "user");
+
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("class", "user-img");
+
+        const h2 = document.createElement('h2');
+        h2.textContent = name;
+
+        const pLocation = document.createElement('p');
+        pLocation.textContent = city + ', ' + country;
+
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+
+        mainDiv.appendChild(divProfile);
+        mainDiv.appendChild(divPortrait);
+        
+        divPortrait.appendChild(img);
+        divProfile.appendChild(h2);
+        divProfile.appendChild(pLocation);
+        divProfile.appendChild(pTagline);
+        // mainDiv.innerHTML = div;
+        return(mainDiv);
+    }
+
+    function getPhotographerMedia() {
+
+        const photoDiv = document.createElement('div');
+        photoDiv.setAttribute('class', 'photographer-media');
+        // .innerHTML = data.map((image) => 
+        //     <img class="photographer-image" src="assets/photographers/${}"></img>
+        // );
+        
+
+        
+        const img = document.createElement('img');
+        img.setAttribute("src", media);
+        img.setAttribute("class", 'thumb-img');
+       
+        photoDiv.appendChild(img);
+        return(photoDiv);
+    }
+
+    return { name, picture, getUserCardDOM, getPhotographerMeta, getPhotographerMedia }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,69 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+let params = new URLSearchParams(window.location.search).get('id');
+
+
+
+const getPhotographer = async() => {
+    return await fetch('http://127.0.0.1:5501/data/photographers.json')
+
+    // récupération du fichier json
+    .then(function(result) { return result.json() })
+    // récupération des données de photographers et media
+    .then(function(data){ return data })
+    // affiche une erreur si le fichier n'ai pas trouvé
+    .catch(function(error){ console.log('une erreur fetch' + error)})
+}
+
+async function displayData(photographers, media) {
+    // console.log(photographers);
+    // console.log(media);
+
+    // recupere l'element parent
+    const photographersSection = document.querySelector("#photograph-header");
+    const photographersMedia = document.querySelector(".photographer_image");
+
+
+    photographers.forEach((photographer) => {
+    //    console.log(photographer);
+        if(photographer.id == params){
+            //recupere les elements de la factory
+            const photographerModel = photographerFactory(photographer);
+
+            // recupere les elements a inserer dans le HTML
+            const getPhotographerMeta = photographerModel.getPhotographerMeta();
+
+            // insere les elements a inserer de la factory dans l'element parent
+            photographersSection.appendChild(getPhotographerMeta);
+        }
+    });
+
+    photographers.forEach((media) => {
+        // console.log(media.photographerId);
+        // console.log(photographer.id);
+
+        if(media.photographerId == params){
+            // console.log(media);
+            const photographerModel = photographerFactory(media);
+            // console.log(photographerModel);
+
+            // recupere les elements a inserer dans le HTML
+            const getPhotographerMedia = photographerModel.getPhotographerMedia();
+
+            // insere les elements a inserer de la factory dans l'element parent
+            photographersMedia.appendChild(getPhotographerMedia);
+        }
+    });
+    
+};
+
+async function init() {
+    // Récupère les datas des photographes et les medias
+    const photographers  = await getPhotographer();
+    // console.log(photographers);
+    displayData(photographers.photographers);
+    displayData(photographers.media);
+    
+};
+
+init();
